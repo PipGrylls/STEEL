Fixed Morphology ratios by using proper weighting

Scripts/SMHMRelation.py
@@ -12,74 +12,7 @@ import matplotlib.pyplot as plt
 import weighted
 from matplotlib.collections import LineCollection
 from matplotlib.colors import ListedColormap, BoundaryNorm
-
-# Loading the catalog
-#Header=['galcount',
-#        'finalflag',
-#        'z',
-#        'Vmaxwt',
-#        'MsMendSerExp',
-#        'AbsMag',
-#        'logReSerExp',
-#        'BT',
-#        'n_bulge',
-#        'NewLCentSat',
-#        'NewMCentSat' ,
-#        'MhaloL',
-#        'probaE',
-#        'probaEll',
-#        'probaS0',
-#        'probaSab',
-#        'probaScd',
-#        'TType',
-#        'P_S0',
-#        'veldisp',
-#        'veldisperr',
-#        'raSDSS7',
-#df = pd.read_csv("/home/ssp1e17/Documents/STEEL/Data/Observational/Lorenzo_SDSS/new_catalog_cModel.dat", delim_whitespace=True)
-
-#Header = ["galcount",
-#          "z",
-#          "Vmaxwt",
-#          "MsMendSerExp",
-#          "AbsMag",
-#          "logReSerExp",
-#          "BT",
-#          "n_bulge",
-#          "NewLCentSat",
-#          "NewMCentSat",
-#          "MhaloL",
-#          "probaE",
-#          "probaEll",
-#          "probaS0",
-#          "probaSab",
-#          "probaScd",
-#          "TType",
-#          "AbsMagCent",
-#          "MsCent",
-#          "veldisp",
-#          "veldisperr",
-#          "AbsModel_newKcorr",
-#          "LCentSat",
-#          "raSDSS7",
-#          "decSDSS7",
-#          "Z",
-#          "sSFR",
-#          "FLAGsSFR",
-#          "MEDIANsSFR",
-#          "P16sSFR",
-#          "P84sSRF", 
-#          "SFR",
-#          "FLAGSFR",
-#          "MEDIANSFR",
-#          "P16SFR",
-#          "P84SRF",
-#          "RA_SDSS",
-#          "DEC_SDSS",
-#          "Z_2",
-#          "Seperation"]
-#df = pd.read_csv("/home/ssp1e17/Documents/STEEL/Data/Observational/Bernardi_SDSS/new_catalog_SFRs.dat", header = None, names = Header, skiprows = 1, delim_whitespace = True)
-
+import matplotlib as mpl
 
 Header=['galcount',
         'finalflag',
@@ -120,23 +53,17 @@ df = df.dropna()
 df = df[df.MsMendSerExp > 0.]
 df = df[df.NewLCentSat == 1.]
 
-# Splitting data into relevant morphologies
+# Splitting data into relevant morphologies using TTypes and P_S0 values
 EarlyGalaxy = df[df.TType <= 0.]
 LateGalaxy = df[df.TType > 0.]
 EllipticalGalaxy = EarlyGalaxy[EarlyGalaxy.P_S0 < 0.5]
 LenticularGalaxy = EarlyGalaxy[EarlyGalaxy.P_S0 >= 0.5]
 
-HM_Len = LenticularGalaxy.MhaloL
-HM_Ell = EllipticalGalaxy.MhaloL
-HM_Late = LateGalaxy.MhaloL
-
-SM_Len = LenticularGalaxy.MsMendSerExp
-SM_Ell = EllipticalGalaxy.MsMendSerExp
-SM_Late = LateGalaxy.MsMendSerExp
-
-
+# Making Halo Mass bins
 hm_binwidth = 0.2
-hm_bins = np.arange(10,15,hm_binwidth)
+hm_bins = np.arange(8,15,hm_binwidth)
+
+# Arrays of zeroes to fill with data
 SM_Ell = np.zeros(len(hm_bins))
 SM_Len = np.zeros(len(hm_bins))
 SM_Late = np.zeros(len(hm_bins))
@@ -183,15 +110,17 @@ for i in range(len(hm_bins)):
     if numtempEll.empty:
         Num_Ell = float('NaN')
     else:
-        Num_Ell = weighted.median(numtempEll, wtmpEll)
+        Num_Ell = np.sum(wtmpEll)
+
     if numtempLen.empty:
         Num_Len = float('NaN')
     else:
-        Num_Len = weighted.median(numtempLen, wtmpLen)
+        Num_Len = np.sum(wtmpLen)
+
     if numtempLate.empty:
         Num_Late = float('NaN')
     else:
-        Num_Late = weighted.median(numtempLate, wtmpLate)
+        Num_Late = np.sum(wtmpLate)
     Total = Num_Ell + Num_Len + Num_Late
     
     # Fraction of Each Morphpology
@@ -201,33 +130,55 @@ for i in range(len(hm_bins)):
         
 
 # Generating Plot with varying line color
-x1 = hm_bins[8:-1]
-y1 = SM_Ell[8:-1]
-fractionEll = Frac_Ell[8:-1] # using these indices to avoid NaN values
+# From Matplotlib documentation
+
+mpl.rcParams.update(mpl.rcParamsDefault)
+plt.rcParams['ytick.minor.visible']=True
+plt.rcParams['xtick.minor.visible']=True
+plt.rcParams['axes.linewidth']=2
+plt.rcParams['xtick.major.size'] = 5
+plt.rcParams['ytick.major.size'] = 5
+plt.rcParams['xtick.minor.size'] = 3
+plt.rcParams['ytick.minor.size'] = 3
+plt.rcParams['xtick.major.width'] = 1
+plt.rcParams['ytick.major.width'] = 1
+plt.rcParams['xtick.minor.width'] = 1
+plt.rcParams['ytick.minor.width'] = 1
+mpl.rcParams['axes.titlepad'] = 20
+plt.rcParams['font.size']=22
+plt.rcParams['lines.linewidth']=3
+
+
+# First line: Elliptical
+x1 = hm_bins[18:-1]
+y1 = SM_Ell[18:-1]
+fractionEll = Frac_Ell[18:-1] # using these indices to avoid NaN values
 
 points1 = np.array([x1, y1]).T.reshape(-1, 1, 2)
 segments1 = np.concatenate([points1[:-1], points1[1:]], axis=1)
 
-x2 = hm_bins[8:-1]
-y2 = SM_Len[8:-1]
-fractionLen = Frac_Len[8:-1]
+# Second line: Lenticular
+x2 = hm_bins[18:-1]
+y2 = SM_Len[18:-1]
+fractionLen = Frac_Len[18:-1]
 
 points2 = np.array([x2, y2]).T.reshape(-1, 1, 2)
 segments2 = np.concatenate([points2[:-1], points2[1:]], axis=1)
 
-
-x3 = hm_bins[8:-1]
-y3 = SM_Late[8:-1]
-fractionLate = Frac_Late[8:-1]
+# Third line: Spiral
+x3 = hm_bins[18:-1]
+y3 = SM_Late[18:-1]
+fractionLate = Frac_Late[18:-1]
 
 points3 = np.array([x3, y3]).T.reshape(-1, 1, 2)
 segments3 = np.concatenate([points3[:-1], points3[1:]], axis=1)
 
+
+# making an array to get limits of colorbars
 lims = np.array([fractionEll.min(), fractionLen.min(), fractionLate.min(), fractionEll.max(), fractionLen.max(), fractionLate.max()])
 
 fig, axs = plt.subplots(1, 1, sharex=True, sharey=True)
-
-MAP = 'tab20c'
+MAP = 'viridis'
 
 # Create a continuous norm to map from data points to colors
 norm1 = plt.Normalize(lims.min(), lims.max())
@@ -238,14 +189,14 @@ lc1.set_linewidth(3)
 line1 = axs.add_collection(lc1)
 
 norm2 = plt.Normalize(lims.min(), lims.max())
-lc2 = LineCollection(segments2, cmap=MAP, norm = norm2, linestyle = ':', label = 'Lenticular')
+lc2 = LineCollection(segments2, cmap=MAP, norm = norm2, linestyle = '--', label = 'Lenticular')
 # Set the values used for colormapping
 lc2.set_array(fractionLen)
 lc2.set_linewidth(3)
 line2 = axs.add_collection(lc2)
 
 norm3 = plt.Normalize(lims.min(), lims.max())
-lc3 = LineCollection(segments3, cmap=MAP, norm = norm3, linestyle = '--', label = 'Spiral')
+lc3 = LineCollection(segments3, cmap=MAP, norm = norm3, linestyle = ':', label = 'Spiral')
 # Set the values used for colormapping
 lc3.set_array(fractionLate)
 lc3.set_linewidth(3)
@@ -258,15 +209,7 @@ axs.set_xlim(x1.min()-0.4, x1.max()+0.4)
 axs.set_ylim(y1.min()-0.4, y1.max()+0.4)
 plt.ylabel("$log_{10}$ $M_*$ [$M_\odot$]")
 plt.xlabel("$log_{10}$ $M_h$ [$M_\odot$]")
-plt.legend()
-plt.show()
-
-
-
-#plt.plot(hm_bins, SM_Ell, label = 'Elliptical')
-#plt.plot(hm_bins, SM_Len, label = 'Lenticular')
-#plt.plot(hm_bins, SM_Late, label = 'Spiral')
-#plt.legend()
-#plt.ylabel("$log_{10}$ $M_*$ [$M_\odot$]")
-#plt.xlabel("$log_{10}$ $M_h$ [$M_\odot$]")
-#plt.show()
+plt.legend(loc = 'upper left', fontsize = 16, frameon=False)
+plt.tight_layout()
+plt.savefig('./../Figures/Paper2/PipSideProject.png')
+plt.savefig('./../Figures/Paper2/PipSideProject.pdf')